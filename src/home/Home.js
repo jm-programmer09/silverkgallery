97fa47e0f95c7890bbef3fc1437dc5d7fe8becc8
Jack <122249962+jm-programmer09@users.noreply.github.com>
2024-09-09 11:27:21 +1000

    Update Home.js

--- src/home/Home.js
+++ src/home/Home.js
@@ -24,7 +24,7 @@ export default function Home () {
         <figure>
           <figcaption>
             <h2>Piece of the Week</h2>
-            <h3>The Engine of Imagination</h3/>
+            <h3>The Engine of Imagination</h3>
             <p> Walt Disney had a long fascination with trains.
 His father and an uncle had spent time working on railroads, and as a teen in Kansas City Walt did a brief stint selling newspapers and snacks on trains.  Later, Walt constructed elaborate model train sets as a way to unwind from the stress of his job. In the late 1940s, he built himself a one-eighth-scale steam locomotive, and after moving into a new home in the Holmby Hills section of Los Angeles in 1950 he laid half a mile of tracks around the property for his railroad. He would dress up in a train engineer’s clothing and give visitors rides on his Carolwood Pacific Railroad, named for the street he lived on.
 His passion for trains is reflected at Disneyland, which has been home to its own railroad since opening in 1955.</p>

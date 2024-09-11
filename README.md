b571f4657462457986e41c36571e94a6dc7db577
jm-programmer09 <jackmellett2009@gmail.com>
2024-09-12 09:17:25 +1000

    morning build - added new animations and fixed sizing for cards

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 - For the BrowserRouter basename, change it depending on which site it is being hosted at
 - Image src's need to start with process.env.PUBLIC_URL
 
-Hours: 210;
+Hours: 214;
--- src/global/modules.js
+++ src/global/modules.js
@@ -1,6 +1,8 @@
 import "./card.css";
 import { Link } from "react-router-dom";
 import data from "../products.json";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 // For the search icon
 export function SearchIcon ({ classname, size = 24 }) {
@@ -11,21 +13,80 @@ export function SearchIcon ({ classname, size = 24 }) {
   );
 }
 
+// Make a function in here so that when it is given the size of the image, if there is no direct size, then state the edition size
+export function sizeParser (dimensions) { // dimensions === data.__item__.dimensions
+  // This is for if there are dimensions in inches
+  if (typeof dimensions[0] === "object") {
+    return (
+      // For if there are multiple dimensions
+      (typeof dimensions[0][0] === "object" ?
+        dimensions[0].map((dimension, index) => (
+          <span key={index}>{dimension[0]}" x {dimension[1]}"&ensp;</span>
+        )) 
+        : <span>{dimensions[0][0]}" x {dimensions[0][1]}"</span> 
+      )
+    );
+  } 
+  // Else for the cards that have no initial specific dimension, then check for other sizes
+  for (let index = 1; index < dimensions.length; index++) {
+    const current_edition = dimensions[index];
+
+    if (current_edition !== 0) {
+      let title_index;
+
+      switch (index) {
+        case 2:
+          title_index = "Paper Edition Size: ";
+          break;
+        case 3:
+          title_index = "Glicee Edition Size: ";
+          break;
+        case 4:
+          title_index = "Canvas Edition Size: ";
+          break;
+        default:
+          title_index = "Edition Size: ";
+      }
+
+      return (
+        <span>{title_index} {current_edition}</span>
+      );
+    }
+  }
+
+  // If it fails to have an edition size just dont say anything or you can add a return here that says 'no specfied size'
+}
+
 // The card design
 export function Card ({ id}) {
+  // For adding the cool animation
+  const cardRef = useRef(null);
+  const cardInView = useInView(cardRef, { once: true });
+
+
   const element = data[id.split("/")[0]][id.split("/")[1]][id.split("/")[2]];
   
   return (
+    <motion.div 
+      initial={{ opacity: 0.1 }}
+      animate={cardInView ? {opacity: 1 } : { opacity: 0}}
+      transition={{
+        duration: 0.4,
+        ease: [0.16, 1, 0.3, 1],
+        delay: Math.random() * 0.2
+      }}
+    >
     <Link to={"/collection/" + id} className="global-card">
-      <div className="imgP">
+      <div className="imgP"       ref={cardRef}>
         <img src={process.env.PUBLIC_URL + "/img/" + (id.split("/")[0] + "/" + id.split("/")[1]) + element.image} alt={id.split("/")[2]}  />
       </div>
 
       <div className="text">
         <h4>{element.title}</h4>
-        <small>{element.dimensions}</small>
+        <small>{sizeParser(element.dimensions)}</small>
         <p>{element.type}</p>
       </div>
     </Link>
+    </motion.div>
   );
 }

42fe21ce838c3d22cced63be9185c3394cfcbacc
jm-programmer09 <jackmellett2009@gmail.com>
2024-11-22 18:35:55 +1000

    finished the errors page

--- src/global/NotFound.js
+++ src/global/NotFound.js
@@ -1,29 +1,68 @@
 import "./notfound.css";
 import { Link } from "react-router-dom";
+import { motion, useInView} from 'framer-motion';
+import { useRef } from "react";
 
 export default function NotFound () {
+  const notfoundRef = useRef(null);
+  const isTextInView = useInView(notfoundRef, {once: true });
+
   return (
     <>
-      <section className="notfound">
-        <h1><span style={{ color: "var(--primary)"}} >404 Error</span> - Page not found</h1>
-
-        <img width={500} src={process.env.PUBLIC_URL + "/img/notfound_image.gif"} />
-
-        <p>Even bugs bunny is confused</p>
-
-        <h2>Find what you are looking for:</h2>
-
-        <div className="button-row">
-          <Link to={"/contactus"}>
-            Contact us
-          </Link>
-
+      <section className="notfound" ref={notfoundRef}>
+        <div className="text">
+          <motion.h1
+          initial={{ opacity: 0 }}
+              animate={isTextInView ? { opacity: 1 } : { opacity: 0 }}
+              transition={{ 
+                duration: 1, 
+                ease: [0.16, 1, 0.3, 1], // cubic-bezier
+                delay: 0
+              }}
+          
+          >Oops! The page you were looking for doesn't exist</motion.h1>
+          <motion.p
+          initial={{ opacity: 0 }}
+              animate={isTextInView ? { opacity: 1 } : { opacity: 0 }}
+              transition={{ 
+                duration: 1, 
+                ease: [0.16, 1, 0.3, 1], // cubic-bezier
+                delay: 0.1
+              }}
+          
+          >You may have misstyped the address or the page may have moved.</motion.p>
+          
+          <motion.h4
+          initial={{ opacity: 0 }}
+              animate={isTextInView ? { opacity: 1 } : { opacity: 0 }}
+              transition={{ 
+                duration: 1, 
+                ease: [0.16, 1, 0.3, 1], // cubic-bezier
+                delay: 0.2
+              }}
+          
+          >Find what you are looking for...</motion.h4>
 
           <Link to={"/collection"}>
-            Our Collection
+              See Our Collection
           </Link>
 
+          {/* Have an after image of an arrow pointing to the button */}
+     
+        </div>
 
+        <div className="image">
+          <motion.img
+          initial={{ opacity: 0, x: 100 }}
+          animate={isTextInView ? { opacity: 1, x: 0 } : { opacity: 0, x: 100 }}
+          transition={{ 
+            duration: 1, 
+            ease: [0.16, 1, 0.3, 1], // cubic-bezier
+          }}
+          
+          width={500} loading="eager" src={process.env.PUBLIC_URL + "/img/notfound_image.gif"} />
+          {/* You even have bugs bunny bamboozled */}
+        
         </div>
 
 
